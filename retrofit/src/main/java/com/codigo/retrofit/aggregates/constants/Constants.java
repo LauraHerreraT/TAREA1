715f844a8cca37d4420e2680bce75bb8713eccd8
LauraHerreraT <herreratorres97@gmail.com>
2025-05-05 20:48:14 -0500

    Delete retrofit directory

--- retrofit/src/main/java/com/codigo/retrofit/aggregates/constants/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.codigo.retrofit.aggregates.constants;
-
-public class Constants {
-    public static final String STATUS_ACTIVE = "ACTIVE";
-    public static final String STATUS_INACTIVE = "INACTIVE";
-
-    public static final String USER_ADMIN = "PRODRIGUEZ";
-}
